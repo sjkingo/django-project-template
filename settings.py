72aa8965f339a6e43040dd626707d5f6c4eb2385
Sam Kingston <sam@sjkwi.com.au>
2015-04-13 13:34:45 +1000

    Set BASE_DIR to absolute path to remove relative parts

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
