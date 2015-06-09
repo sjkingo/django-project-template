7d015779de61ab753de0de563c91f98afc855363
Sam Kingston <sam@sjkwi.com.au>
2015-06-10 08:45:20 +1000

    Template now uses a settings app with dev and prod files, instead of settings/local_settings

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+    if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+        print('DJANGO_SETTINGS_MODULE not set, defaulting to settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.dev')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
